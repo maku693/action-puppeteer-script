aa77e952f708e19b510f6721634883071a21ad73
maku693 <me@maku693.net>
2022-10-13 22:34:53 +0900

    Download and cache Chromium on the runner side

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+const fs = require("fs/promises");
+const path = require("path");
 const core = require("@actions/core");
+const tc = require("@actions/tool-cache");
 const puppeteer = require("puppeteer");
+const { fetch } = require("undici");
 
 async function main() {
   const script = core.getInput("script", { required: true });
 
-  const browser = await puppeteer.launch();
+  const chromiumPath = await findOrDownloadChromium();
+  const browser = await puppeteer.launch({
+    executablePath: chromiumPath,
+  });
 
   const args = { browser };
 
@@ -14,6 +21,52 @@ async function main() {
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+async function findOrDownloadChromium() {
+  core.info("Fetching latest chrome revison");
+  const latestRevision = await (async () => {
+    const res = await fetch(
+      "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2FLAST_CHANGE?alt=media"
+    );
+    return res.text();
+  })();
+  core.info(`Latest Chromium revision: ${latestRevision}`);
+
+  // Download and create cache if cache is not found
+  let cachedPath = tc.find("chromium", latestRevision);
+  if (cachedPath) {
+    core.info("Using cached Chromium");
+  } else {
+    core.info(`Chromium cache not found; Downloading`);
+    const downloadPath = path.resolve(__dirname, "tmp");
+    const revisionInfo = await puppeteer
+      .createBrowserFetcher({
+        path: downloadPath,
+      })
+      .download(latestRevision, (x, y) => {
+        core.debug(`Download progress: ${x}/${y}`);
+      });
+    core.info("Download finished!");
+
+    core.debug("Revision info: ", revisionInfo);
+
+    // Create symlink to launch the executable easily
+    await fs.symlink(
+      revisionInfo.executablePath,
+      path.join(revisionInfo.folderPath, "chromium")
+    );
+
+    cachedPath = await tc.cacheDir(
+      revisionInfo.folderPath,
+      "chromium",
+      latestRevision
+    );
+  }
+
+  core.debug(`Cached path: ${cachedPath}`);
+
+  return path.join(cachedPath, "chromium");
 }
 
 const AsyncFunction = (async () => {}).constructor;
